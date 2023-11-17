3c5a532a94f6920ea00ed754d9d05f534accdd44
Lena Zhang <lena.zhang@tpisoftware.com>
2023-11-17 14:07:03 -0800

    Lena: update views.py, models.py

--- flask-server/api/models.py
+++ flask-server/api/models.py
@@ -7,10 +7,20 @@ class Movie(db.Model):
     title = db.Column(db.String(255))
     year = db.Column(db.String(4))
     imdb_id = db.Column(db.String(20))
-    poster = db.Column(db.String(255))
+    # poster = db.Column(db.String(255))
 
     def __init__(self, title, year, imdb_id, poster):
         self.title = title
         self.year = year
         self.imdb_id = imdb_id
         self.poster = poster
+
+class Favorite(db.Model):
+    imdb_id = db.Column(db.String(20), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.String(4))
+    
+    def __init__(self, imdb_id, title, year):
+        self.title = title
+        self.year = year
+        self.imdb_id = imdb_id
--- flask-server/api/views.py
+++ flask-server/api/views.py
@@ -1,22 +1,98 @@
 from flask import Blueprint, jsonify, request
 import requests
 from . import db
-from .models import Movie 
+from .models import Movie, Favorite
 from . import omdb
 
 main = Blueprint('main', __name__)
 
-def fetch_and_store_movies():
-    api_url = f"{omdb.base_url}?s=Marvel&apikey={omdb.api_key}"
+## Lena(Yu-Lun) Feature
+def fetch_from_movies(query):
+    # Encode query for URL
+    url_query = requests.utils.quote(query)
+    api_url = f"{omdb.base_url}?s={url_query}&apikey={omdb.api_key}"
     response = requests.get(api_url)
     
     if response.status_code == 200:
         data = response.json()
-        store_data_in_database(data)
-        return data
+        # store_data_in_database(data)
+        if 'Search' in data:
+            movies = []
+            for movie in data['Search']:
+                # Ensure you access the keys correctly based on the JSON response structure
+                print(f"Id: {movie['imdbID']}, Title: {movie['Title']}, Year: {movie['Year']}")
+                #  These dictionaries are appended to the movies list. The use of movie.get('imdbID', '') and similar lines ensures that if a particular key is not found in the movie data, a default empty string ('') is used instead
+                movies.append({
+                    'imdbID': movie.get('imdbID', ''),
+                    'title': movie.get('Title', ''),
+                    'year': movie.get('Year', ''),
+                })
+            
+            return jsonify(movies), 200
+        else:
+            print("No search results found.")
+            return jsonify({'error': 'No search results found'}), 404
     else:
-        return None
-    
+        return jsonify({'error': 'Failed to fetch data'}), response.status_code
+
+@main.route('/search_movies', methods=['GET'])
+def search_movies():
+    # Get the search query from the request
+    query = request.args.get('query', '')
+    return fetch_from_movies(query)
+
+# fetch_from_movies("spider man")
+
+# 加入我的做愛 Table
+def add_movie_to_favorites(imdb_id):
+    # Check if the movie is already in the favorites
+    if Favorite.query.filter_by(imdb_id=imdb_id).first():
+        return {'error': 'Movie already in favorites'}, 409
+
+    # Check if movie exists in the Movie table
+    movie = Movie.query.filter_by(imdb_id=imdb_id).first()
+    if not movie:
+        return {'error': 'Movie not found'}, 404
+
+    # Add the movie to favorites
+    new_favorite = Favorite(imdb_id=movie.imdb_id, title=movie.title, year=movie.year)
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    return {'message': 'Movie added to favorites'}, 201
+
+@main.route('/add_to_favorites', methods=['POST'])
+def add_to_favorites_endpoint():
+    if not request.is_json:
+        return jsonify({'error': 'Invalid JSON data'}), 400
+
+    data = request.get_json()
+    imdb_id = data.get('imdb_id')
+
+    result, status_code = add_movie_to_favorites(imdb_id)
+    return jsonify(result), status_code
+
+# 瀏覽我的最愛 movie
+@main.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = Favorite.query.all()
+    for fav in favorites:
+        favorite_movies = [
+            {'imdb_id': fav.imdb_id, 'title': fav.title, 'year': fav.year}
+        ]
+    return jsonify(favorite_movies), 200
+
+# 從我的最愛刪除 movie
+@main.route('/remove_from_favorites/<imdb_id>', methods=['DELETE'])
+def remove_from_favorites(imdb_id):
+    favorite = Favorite.query.filter_by(imdb_id=imdb_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Movie removed from favorites'}), 200
+    else:
+        return jsonify({'error': 'Movie not found in favorites'}), 404
+
 # 稍待
 # @main.route('/favorite_movie', methods=['POST'])
 # def favorite_movie():
