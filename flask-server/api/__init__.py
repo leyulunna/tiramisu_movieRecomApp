dc0fdb0c826ca55a470741e0b7e198e7d73c0ceb
Lena Zhang <lena.zhang@tpisoftware.com>
2024-02-12 16:10:54 -0800

    heroku deployment

--- flask-server/api/__init__.py
+++ flask-server/api/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import logging
 import os
+import psycopg2
 
 db = SQLAlchemy()
 
@@ -13,7 +14,15 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://yjwipptkfagjrk:60849b67bf32dba8da4f4e45ac0bc174b04627508f425e21d83048a727e5d6f5@ec2-34-236-199-229.compute-1.amazonaws.com:5432/d1b7i31omjl47r')
+
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
     db.init_app(app)
