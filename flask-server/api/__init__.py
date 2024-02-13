1bdee88671668c04329493311e09dfdb17b4ea51
Lena Zhang <lena.zhang@tpisoftware.com>
2024-02-12 16:31:03 -0800

    delete: psycopg2

--- flask-server/api/__init__.py
+++ flask-server/api/__init__.py
@@ -15,13 +15,9 @@ def create_app():
     CORS(app)
 
     DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://yjwipptkfagjrk:60849b67bf32dba8da4f4e45ac0bc174b04627508f425e21d83048a727e5d6f5@ec2-34-236-199-229.compute-1.amazonaws.com:5432/d1b7i31omjl47r')
 
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
